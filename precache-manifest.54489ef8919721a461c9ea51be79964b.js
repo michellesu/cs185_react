1773c63797bd999cea7645f277717b3187c0a063
Michelle Su <michellesu@umail.ucsb.edu>
2020-04-26 16:44:26 -0400

    Updates

---
+++ precache-manifest.54489ef8919721a461c9ea51be79964b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70947616123c57966acdb4b3a0417549",
+    "url": "/cs185_react/index.html"
+  },
+  {
+    "revision": "80209d882864cfd1c541",
+    "url": "/cs185_react/static/css/main.019320e4.chunk.css"
+  },
+  {
+    "revision": "a67179da01af693a9710",
+    "url": "/cs185_react/static/js/2.83c988d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cs185_react/static/js/2.83c988d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80209d882864cfd1c541",
+    "url": "/cs185_react/static/js/main.04e13d4f.chunk.js"
+  },
+  {
+    "revision": "078806fda7dcfa3021c2",
+    "url": "/cs185_react/static/js/runtime-main.58eafaf2.js"
+  },
+  {
+    "revision": "98050207802c3355a70367854eb81b46",
+    "url": "/cs185_react/static/media/bodyimg.98050207.jpeg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/cs185_react/static/media/githubLogo.76d344f0.svg"
+  },
+  {
+    "revision": "4f2a4102f4d117dffd5723bef732b4e3",
+    "url": "/cs185_react/static/media/img1.4f2a4102.jpg"
+  },
+  {
+    "revision": "f84f4c534f009ff0167fb2e7882c0161",
+    "url": "/cs185_react/static/media/img2.f84f4c53.jpg"
+  },
+  {
+    "revision": "a880d8ab66572550c44827ed3664d19c",
+    "url": "/cs185_react/static/media/img3.a880d8ab.jpg"
+  },
+  {
+    "revision": "0c45a51991806058a662a0ed1b99eb92",
+    "url": "/cs185_react/static/media/img4.0c45a519.jpg"
+  },
+  {
+    "revision": "e496b628e4464a8535472782068c026a",
+    "url": "/cs185_react/static/media/img5.e496b628.jpg"
+  },
+  {
+    "revision": "db0c3ad43cf04daa223bd47b50fda6e5",
+    "url": "/cs185_react/static/media/img6.db0c3ad4.jpg"
+  },
+  {
+    "revision": "1adeb9576efaf45278208ac301a6cd90",
+    "url": "/cs185_react/static/media/mapofthings.1adeb957.png"
+  },
+  {
+    "revision": "e035007e165c4b4490fa77351efea946",
+    "url": "/cs185_react/static/media/shopmystyle.e035007e.png"
+  },
+  {
+    "revision": "9514612b5388b94e210e4362636e5bee",
+    "url": "/cs185_react/static/media/vid1.9514612b.mp4"
+  },
+  {
+    "revision": "1420b46f8d523fce0f25c96aa7c712b6",
+    "url": "/cs185_react/static/media/vid2.1420b46f.mp4"
+  },
+  {
+    "revision": "978e8b53b588b32e3a081d05c0398f5f",
+    "url": "/cs185_react/static/media/vid3.978e8b53.mp4"
+  },
+  {
+    "revision": "9ebd8eb1f561ea1bf490f5d38391e938",
+    "url": "/cs185_react/static/media/vid4.9ebd8eb1.mp4"
+  }
+]);
